0b4d7099e8fdb7db533de6515b8675304959b315
Ferrisama <94129345+Ferrisama@users.noreply.github.com>
2023-09-10 19:26:09 +0530

    Update index.ts

--- utils/index.ts
+++ utils/index.ts
@@ -1,26 +1,19 @@
 import { CatProps, FilterProps } from "@types";
 
 export const updateSearchParams = (type: string, value: string) => {
-  // Get the current URL search params
   const searchParams = new URLSearchParams(window.location.search);
-
-  // Set the specified search parameter to the given value
   searchParams.set(type, value);
 
-  // Set the specified search parameter to the given value
   const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
 
   return newPathname;
 };
 
 export const deleteSearchParams = (type: string) => {
-  // Set the specified search parameter to the given value
   const newSearchParams = new URLSearchParams(window.location.search);
 
-  // Delete the specified search parameter
   newSearchParams.delete(type.toLocaleLowerCase());
 
-  // Construct the updated URL pathname with the deleted search parameter
   const newPathname = `${
     window.location.pathname
   }?${newSearchParams.toString()}`;
@@ -31,13 +24,11 @@ export const deleteSearchParams = (type: string) => {
 export async function fetchCats(filters: FilterProps) {
   const { name, origin, playfulness } = filters;
 
-  // Set the required headers for the API request
   const headers: HeadersInit = {
     "X-RapidAPI-Key": "5bde3ed85bmsh5d4d26b2ec2e4dep16f445jsnfa0957af9f76",
     "X-RapidAPI-Host": "cats-by-api-ninjas.p.rapidapi.com",
   };
 
-  // Set the required headers for the API request
   const response = await fetch(
     `https://cats-by-api-ninjas.p.rapidapi.com/v1/cats?name=${name}&origin=${origin}&playfulness=${playfulness}`,
     {
@@ -45,7 +36,6 @@ export async function fetchCats(filters: FilterProps) {
     }
   );
 
-  // Parse the response as JSON
   const result = await response.json();
 
   return result;
